680bfcf88db969c5880eab70f99e1caf27c97add
likanug <likanug.g@qq.com>
2025-09-06 01:21:37 +0800

    feat: 增加"/api/anis/:id"的路由API，进行数据库的查询

--- build.zig
+++ build.zig
@@ -91,8 +91,13 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const pg = b.dependency("pg", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
     exe.root_module.addImport("httpz", httpz.module("httpz"));
+    exe.root_module.addImport("pg", pg.module("pg"));
 
     // This declares intent for the executable to be installed into the
     // install prefix when running `zig build` (i.e. when executing the default
--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,26 @@
 const std = @import("std");
 const httpz = @import("httpz");
+const pg = @import("pg");
+
+const App = struct {
+    db: *pg.Pool,
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
+    // 初始化数据库连接池
+    var db = try init_db_pool(allocator);
+    defer db.deinit();
+
+    var app = App {
+        .db = db,
+    };
     // More advance cases will use a custom "Handler" instead of "void".
     // The last parameter is our handler instance, since we have a "void"
     // handler, we passed a void ({}) value.
-  var server = try httpz.Server(void).init(allocator, .{.port = 5882}, {});
+    var server = try httpz.Server(*App).init(allocator, .{.port = 5882}, &app);
     defer {
         // clean shutdown, finishes serving any live request
         server.stop();
@@ -17,20 +29,53 @@ pub fn main() !void {
 
     var router = try server.router(.{});
     router.get("/", index, .{});
-    router.get("/api/user/:id", getUser, .{});
+    router.get("/api/anis/:id", getAniInfo, .{});
+
+
     std.debug.print("server listening: http://localhost:{?}\n", .{ server.config.port });
     // blocks
     try server.listen();
 }
 
-fn index(_: *httpz.Request, res: *httpz.Response) !void {
+pub fn init_db_pool(allocator: std.mem.Allocator) !*pg.Pool {
+    return try pg.Pool.init(allocator, .{
+        .size = 5,
+        .connect = .{
+            .port = 5432,
+            .host = "127.0.0.1",
+        },
+        .auth = .{
+            .username = "postgres",
+            .database = "newsletter",
+            .password = "password",
+            .timeout = 10_000,
+        },
+    });
+}
+
+
+fn index(_: *App, _: *httpz.Request, res: *httpz.Response) !void {
     res.body =
     \\<!DOCTYPE html>
     \\ <h1>Hello this ani-z</h1>
     ;
 }
 
-fn getUser(req: *httpz.Request, res: *httpz.Response) !void {
+fn getAniInfo(app: *App, req: *httpz.Request, res: *httpz.Response) !void {
     res.status = 200;
-    try res.json(.{.id = req.param("id").?, .name = "Teg"}, .{});
+    const ani_id = req.param("id").?;
+
+    var row = try app.db.row("select title, detail_url, platform from ani_info where id = $1", .{ani_id}) orelse {
+        res.status = 404;
+        res.body = "Not found";
+        return;
+    };
+    defer row.deinit() catch {};
+
+    try res.json(.{
+        .id = ani_id,
+        .title = row.get([]u8, 0),
+        .detailUrl = row.get([]u8, 1),
+        .platform = row.get([]u8, 2),
+    }, .{});
 }
